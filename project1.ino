36a45cf99a8452f5311df55a83007044b2466afa
Lyons Software <admin@lyonssoftware.com>
2016-10-08 22:54:52 -0500

    Update project1.ino

--- project1.ino
+++ project1.ino
@@ -49,7 +49,8 @@ void setup() {
   // Print a message to the LCD.
   lcd.print("hello, world!");
 }
-
+ 
+// start loop 
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
