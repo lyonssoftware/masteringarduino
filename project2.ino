f87163066611def4736ea80fb360e61987a318c9
Lyons Software <admin@lyonssoftware.com>
2016-10-15 23:15:01 -0500

    Update project2.ino

--- project2.ino
+++ project2.ino
@@ -1,1 +1,30 @@
-// project 2 
+/*
+  Author: Steve Lyons
+  Company: Lyons Software
+  Project: project2 - Blink LED on Breadboard
+  Goal: Use Arduino Uno to blink LED on breadboard 
+*/
+
+  // setup pin 13
+  int led = 13;
+  
+  void setup() 
+  { 
+    // initialize the digital pin as an output
+    pinMode(led, OUTPUT);
+  }
+
+  void loop() 
+  {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(led, HIGH); 
+
+     // wait for 3 seconds
+    delay(3000);
+
+     // turn the LED off by making voltage LOW
+    digitalWrite(led, LOW);
+
+    // wait for 3 seconds
+    delay(3000); 
+  }
