46f8f8895e845ad3cbc192c2824dd04d560d728a
Lyons Software <admin@lyonssoftware.com>
2016-12-10 23:24:53 -0600

    Update project4.ino

--- project4.ino
+++ project4.ino
@@ -5,4 +5,14 @@
   Goal: 
 */
 
-// add arduino code here 
+int led = 3;
+int led2 = 4;
+
+void setup()
+{
+  // setup program o
+  for(int i = 0; i < 10; i++)
+  {
+    // print loop statement 
+  }
+}
